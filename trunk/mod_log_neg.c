795b31d9086bce11c7ecc8c71eabb6645552afc8
ssjkakaroto@gmail.com <ssjkakaroto@gmail.com@adf3b0ed-413f-133d-84a2-f3ce81b74654>
2013-11-01 10:14:20 +0000

    Correção do módulo de persistência de desenvolvedores, após informação do professor.

---
+++ trunk/mod_log_neg.c
@@ -0,0 +1,24 @@
+#define LOGICA_NEGOCIO_SERV
+
+#include "tabela_codigos.h"
+#include "i_estruturas.h"
+#include "i_mod_pers_dev.h"
+#include "i_mod_pers_prod.h"
+#include "i_mod_pers_def.h"
+#include "i_mod_log_neg.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+
+
+
+
+int retorna_desenvolvedor(struct desenvolvedor *dev)
+{
+	int ret;
+	
+	ret = consultar_desenvolvedor(dev);
+	
+	return(ret);
+}
